701c914c3b4ed47343309c9d630ea4d20ca878b7
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-21 16:31:21 -0800

    created and basic structure for projectRouter and actionRouter

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 //imported __Router
+const actionRouter = require('./Routers/actionRouter');
+const projectRouter = require('./Routers/projectRouter');
 
 app.use(express.json());
 
