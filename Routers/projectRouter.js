c8c1b7ffb9d3df5bd7dffa5ce5b95c7360935401
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-21 18:42:31 -0800

    Custom Middlware > pRouter, created but not used yet

--- Routers/projectRouter.js
+++ Routers/projectRouter.js
@@ -101,12 +101,18 @@ router.delete("/projects/:id", (req, res) => {
     });
 });
 
-module.exports = router;
+// Custom Middleware
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+  
+    dbProject.get(id)
+      .then(project => {
+        req.project = project;
+        next();
+      })
+      .catch(error => {
+        res.status(400).json({ message: "Invalid project id." });
+      });
+  }
 
-// X POST	/api/posts	Creates a post using the information sent inside the request body.
-// POST	/api/posts/:id/comments	Creates a comment for the post with the specified id using information sent inside of the request body.
-// X GET	/api/posts	Returns an array of all the post objects contained in the database.
-// X GET	/api/posts/:id	Returns the post object with the specified id.
-// GET	/api/posts/:id/comments	Returns an array of all the comment objects associated with the post with the specified id.
-// X DELETE	/api/posts/:id	Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
-// X PUT	/api/posts/:id	Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+module.exports = router;
